b21c19161e306b6ae93cde66272999b2d65703f2
Ernestas Juška <ernjus@softera.lt>
2023-07-14 20:56:08 +0300

    wip

--- Scripts/CheckSystemResources.ps1
+++ Scripts/CheckSystemResources.ps1
@@ -12,14 +12,20 @@ $FreePhysicalMemoryInBytes = $OS.FreePhysicalMemory * 1KB
 $ResourceErrors = @(
     $RequiredFreeSystemDriveSpaceInBytes = 10000GB
     if ($FreeSystemDriveSpaceInBytes -lt $RequiredFreeSystemDriveSpaceInBytes) {
-        "##[error]There is not enough disk space left. There must be at least $(
-            '{0:0.#,.}' -f ($RequiredFreeSystemDriveSpaceInBytes / 1GB)) GB."
+        -join @(
+            '##vso[task.logissue type=error;]'
+            'There is not enough disk space left. There must be at least '
+            "$('{0:0.#,.}' -f ($RequiredFreeSystemDriveSpaceInBytes / 1GB)) GB."
+        )
     }
 
     $RequiredFreePhysicalMemoryInBytes = 4000GB
     if ($FreePhysicalMemoryInBytes -lt $RequiredFreePhysicalMemoryInBytes) {
-        "##[error]There is not enough operating memory space left. There must be at least $(
-            '{0:0.#,.}' -f ($RequiredFreePhysicalMemoryInBytes / 1GB)) GB."
+        -join @(
+            '##vso[task.logissue type=error;]'
+            'There is not enough operating memory space left. There must be at least '
+            "$('{0:0.#,.}' -f ($RequiredFreePhysicalMemoryInBytes / 1GB)) GB."
+        )
     }
 )
 
